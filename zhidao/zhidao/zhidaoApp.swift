8e870be80031584b5be50fca794f1985e028987f
Zigao Wang <zigao.wang24@gmail.com>
2025-03-01 08:59:08 +0800

    feat: prototype

---
+++ zhidao/zhidao/zhidaoApp.swift
@@ -0,0 +1,17 @@
+//
+//  zhidaoApp.swift
+//  zhidao
+//
+//  Created by Zigao Wang on 3/1/25.
+//
+
+import SwiftUI
+
+@main
+struct zhidaoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
